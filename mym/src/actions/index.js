f4229ad4e1590f6afad2440ead36439f81e957b7
Nicholas Pung <nicholaspung>
2019-06-24 08:36:56 -0700

    initial commit, added majority of npm modules that project needs

---
+++ mym/src/actions/index.js
@@ -0,0 +1,3 @@
+// set action types
+
+// create actions
---
+++ mym/src/reducers/index.js
@@ -0,0 +1,12 @@
+// import actions
+
+const initializeState = {
+
+}
+
+export default function(state = initializeState, action ) {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
