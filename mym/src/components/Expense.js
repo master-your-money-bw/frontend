3feb76b9ef84db0e22b1b4f07a59e656473a93b8
Nicholas Pung <nicholaspung>
2019-06-25 15:31:40 -0700

    added update view on expenselist

--- mym/src/components/Expense.js
+++ mym/src/components/Expense.js
@@ -15,38 +15,51 @@ class Expense extends React.Component {
         this.setState(prevState => ({ updating: !prevState.updating }))
     }
 
+    onInputChange = e => {
+        this.setState({
+            newExpense: {
+                ...this.state.newExpense,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
     render() {
         return (
             <div className="card">
                 <div className="card-content">
                     <div className="row">
-                    {
-                        <div>
+                    {this.state.updating ? 
+                        (<div>
                             <div className="input-field">
-                                <label className="active" htmlFor="date">Date</label>
-                                <input required type="date" onChange={this.onInputChange} name="date" value={this.state.newExpense.expensename} id="date"/>
+                                <label className="active" htmlFor="expensename">Expense Name</label>
+                                <input required type="text" onChange={this.onInputChange} name="expensename" value={this.state.newExpense.expensename} id="expensename"/>
                             </div>
                             <div className="input-field">
-                                <label className="active" htmlFor="date">Date</label>
-                                <input required type="date" onChange={this.onInputChange} name="date" value={this.state.newExpense.category} id="date"/>
+                                <label className="active" htmlFor="category">Category</label>
+                                <input required type="text" onChange={this.onInputChange} name="category" value={this.state.newExpense.category} id="category"/>
                             </div>
                             <div className="input-field">
-                                <label className="active" htmlFor="date">Date</label>
-                                <input required type="date" onChange={this.onInputChange} name="date" value={this.state.newExpense.amount} id="date"/>
+                                <label className="active" htmlFor="amount">Amount</label>
+                                <input required type="number" onChange={this.onInputChange} name="amount" value={this.state.newExpense.amount} id="amount"/>
+                            </div>
+                            <button >update</button>
+                            <button onClick={this.toggleUpdate}>cancel</button>
+                        </div>) : 
+                        (<div>
+                            <div className="col">
+                                <p>Name: {this.props.expense.expensename}</p>
+                                <p>Category: {this.props.expense.category}</p>
+                                <p>Amount: {this.props.expense.amount}</p>
+                            </div>
+                            <div className="col">
+                                <button onClick={this.toggleUpdate}>update</button>
+                            </div>
+                            <div className="col">
+                                <button>delete</button>
                             </div>
                         </div>
-                    }
-                        <div className="col">
-                            <p>Name: {this.props.expense.expensename}</p>
-                            <p>Category: {this.props.expense.category}</p>
-                            <p>Amount: {this.props.expense.amount}</p>
-                        </div>
-                        <div className="col">
-                            <button>update</button>
-                        </div>
-                        <div className="col">
-                            <button>delete</button>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
